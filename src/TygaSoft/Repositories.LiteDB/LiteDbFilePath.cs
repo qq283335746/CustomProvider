3d88c8ad54d40ec96e87944b7880dc067271fc8f
qq283335746 <283335746@qq.com>
2019-08-23 18:00:26 +0800

    GetFullPath test

---
+++ src/TygaSoft/Repositories.LiteDB/LiteDbFilePath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yibi.Repositories.LiteDB
+{
+    public class LiteDbFilePath
+    {
+        //待测试其正确性
+        public static string GetFilePath(string fileName)
+        {
+            var path = System.IO.Path.GetFullPath(
+               AppDomain.CurrentDomain.BaseDirectory +
+               string.Format("../App_Data/{0}.ldb", fileName)
+               );
+
+            return path;
+        }
+    }
+}
